69d605c4fc2d7800c8ce5d176806528daeb7a9a8
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-06-05 08:17:41 +0530

    login page test update

--- tests/loginpage.spec.ts
+++ tests/loginpage.spec.ts
@@ -1,9 +1,26 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../src/pages/LoginPage';
-test('Login Page test', async ({ page }) => {
+test('Login Page title test', async ({ page }) => {
     let loginPage = new LoginPage(page);
     await loginPage.goToLoginPage();
+
     let loginPageTitle = await loginPage.getLoginPageTitle();
     expect(loginPageTitle).toBe('Account Login');
    // loginPage.doLogin('pwbatchtest@open.com', 'pw123');
-});git 
+});
+
+
+test('Login Page Forgotten Password Link test', async ({ page }) => {
+    let loginPage = new LoginPage(page);
+    await loginPage.goToLoginPage();
+
+    expect(await loginPage.isForgotPwdLinkExist()).toBeTruthy();
+   // loginPage.doLogin('pwbatchtest@open.com', 'pw123');
+});
+
+test('Login Test Nominal', async({page})=>{
+let loginPage = new LoginPage(page);
+    await loginPage.goToLoginPage();
+    await loginPage.doLogin('pwbatchtest@open.com', 'pw123');
+
+})
