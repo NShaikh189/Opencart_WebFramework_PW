8158470f63f3ae25a21e5f07955735c878a35c8c
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-06-08 09:07:58 +0530

    loginpage fixture

---
+++ tests/loginpagefixture.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '../src/fixtures/pagefixtures';
+import { beforeEach } from 'node:test';
+
+test.beforeEach(async({loginPage, homePage})=>{
+    await loginPage.goToLoginPage();
+});
+
+
+test('Login Page title test', async ({loginPage} ) => {
+     let loginPageTitle = await loginPage.getLoginPageTitle();
+    expect(loginPageTitle).toBe('Account Login');
+
+});
+
+
+
+test('Login Page Forgotten Password Link test', async ({loginPage}) => { 
+    expect(await loginPage.isForgotPwdLinkExist()).toBeTruthy();
+});
+
+test('Login Test Nominal', async({loginPage, homePage})=>{
+    await loginPage.doLogin('pwbatchtest@open.com', 'pw123');
+    expect(await homePage.getHomePageTitle()).toBe('My Account');
+});
+
+/**
+ * 
+ * page, expect are inbuilt fixtures
+ * In below test method we passed page in the async but not expect
+ * test('',async({page})=>{
+ * expect().toBe();
+ * })
+ * 
+ * expect is not a fixture, is is a globally imported assertion library
+ */
